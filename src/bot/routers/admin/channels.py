a612a6817b7ad24cc90ec719c138041dd5842cfe
progeroffline <progeroffline@gmail.com>
2024-11-12 15:40:40 +0200

    Create custom logs messages for all admin handlers

--- src/bot/routers/admin/channels.py
+++ src/bot/routers/admin/channels.py
@@ -11,6 +11,7 @@ from aiogram.types import (
 from aiogram_i18n import I18nContext
 
 from bot.repositories.channel import ChannelRepository
+from bot.dependencies import logger
 
 
 router = Router(name="channels")
@@ -30,6 +31,13 @@ async def channels(
     i18n: I18nContext,
     state: FSMContext,
 ):
+    logger.info(
+        "Admin opened channels menu, "
+        f"User ID: {message.from_user.id},"  # type: ignore
+        f"Username: {message.from_user.username},"  # type: ignore
+        f"Chat ID: {message.chat.id}, "
+        f"Message: {message.text}"
+    )
     await state.clear()
     channels = await channel_repository.get_all()
     if len(channels) == 0:
@@ -46,6 +54,13 @@ async def channels(
 
 @router.message(Command("channels_remove"))
 async def channels_remove(message: Message, i18n: I18nContext, state: FSMContext):
+    logger.info(
+        "Admin remove channel menu, "
+        f"User ID: {message.from_user.id},"  # type: ignore
+        f"Username: {message.from_user.username},"  # type: ignore
+        f"Chat ID: {message.chat.id}, "
+        f"Message: {message.text}"
+    )
     await state.clear()
     await message.answer(i18n.get("enter_channel_message_to_remove"))
     await state.set_state(LocalStates.enter_channel_message_to_remove)
@@ -66,13 +81,42 @@ async def remove_channel_from_database(
         return
 
     channel = message.forward_origin.chat  # type: ignore
+
+    logger.info(
+        "Admin received channel message to remove, "
+        f"User ID: {message.from_user.id}, "  # type: ignore
+        f"Username: {message.from_user.username}, "  # type: ignore
+        f"Chat ID: {message.chat.id}, "
+        f"Message: {message.text}, "
+        f"Channel ID: {channel.id}, "
+        f"Channel Title: {channel.title}"
+    )
+
     await channel_repository.delete(id=channel.id)
     await message.answer(i18n.get("success_remove_channel_to_database"))
     await state.clear()
 
+    logger.info(
+        "Channel successfully remove by admin request, "
+        f"User ID: {message.from_user.id}, "  # type: ignore
+        f"Username: {message.from_user.username}, "  # type: ignore
+        f"Chat ID: {message.chat.id}, "
+        f"Message: {message.text}, "
+        f"Channel ID: {channel.id}, "
+        f"Channel Title: {channel.title}"
+    )
+
 
 @router.message(Command("channels_add"))
 async def channels_add(message: Message, i18n: I18nContext, state: FSMContext):
+    logger.info(
+        "Admin opened add channel menu, "
+        f"User ID: {message.from_user.id}, "  # type: ignore
+        f"Username: {message.from_user.username}, "  # type: ignore
+        f"Chat ID: {message.chat.id}, "
+        f"Message: {message.text}, "
+    )
+
     await state.clear()
     await message.answer(i18n.get("enter_channel_message_to_add"))
     await state.set_state(LocalStates.enter_channel_message_to_add)
@@ -93,12 +137,42 @@ async def save_channel_to_database(
         return
 
     channel = message.forward_origin.chat  # type: ignore
+
+    logger.info(
+        "Admin received channel message to add, "
+        f"User ID: {message.from_user.id}, "  # type: ignore
+        f"Username: {message.from_user.username}, "  # type: ignore
+        f"Chat ID: {message.chat.id}, "
+        f"Message: {message.text}, "
+        f"Channel ID: {channel.id}, "
+        f"Channel Title: {channel.title}"
+    )
+
     if not await channel_repository.exists(channel.id):  # type: ignore
         await channel_repository.create(id=channel.id, title=channel.title)
         await message.answer(i18n.get("success_save_channel_to_database"))
         await state.clear()
+
+        logger.info(
+            "Channel successfully add by admin request, "
+            f"User ID: {message.from_user.id}, "  # type: ignore
+            f"Username: {message.from_user.username}, "  # type: ignore
+            f"Chat ID: {message.chat.id}, "
+            f"Message: {message.text}, "
+            f"Channel ID: {channel.id}, "
+            f"Channel Title: {channel.title}"
+        )
     else:
         await message.answer(i18n.get("channel_already_added"))
+        logger.info(
+            "Channel add failed cause channel already exists in database, "
+            f"User ID: {message.from_user.id}, "  # type: ignore
+            f"Username: {message.from_user.username}, "  # type: ignore
+            f"Chat ID: {message.chat.id}, "
+            f"Message: {message.text}, "
+            f"Channel ID: {channel.id}, "
+            f"Channel Title: {channel.title}"
+        )
 
 
 @router.message(Command("channels_time"))
@@ -107,6 +181,13 @@ async def channels_time(
     i18n: I18nContext,
     state: FSMContext,
 ):
+    logger.info(
+        "Admin opened channel update time menu, "
+        f"User ID: {message.from_user.id}, "  # type: ignore
+        f"Username: {message.from_user.username}, "  # type: ignore
+        f"Chat ID: {message.chat.id}, "
+        f"Message: {message.text}, "
+    )
     await state.clear()
     await message.answer(i18n.get("enter_channel_message_to_update_time"))
     await state.set_state(LocalStates.enter_channel_message_to_update_time)
@@ -126,7 +207,26 @@ async def enter_channel_message_to_update_time(
     ):
         return
     channel = message.forward_origin.chat  # type: ignore
+
+    logger.info(
+        "Admin received channel message to update time, "
+        f"User ID: {message.from_user.id}, "  # type: ignore
+        f"Username: {message.from_user.username}, "  # type: ignore
+        f"Chat ID: {message.chat.id}, "
+        f"Message: {message.text}, "
+        f"Channel ID: {channel.id}, "
+        f"Channel Title: {channel.title}"
+    )
     if not await channel_repository.exists(channel.id):  # type: ignore
+        logger.info(
+            "Admin update time failed channel not exists in database, "
+            f"User ID: {message.from_user.id}, "  # type: ignore
+            f"Username: {message.from_user.username}, "  # type: ignore
+            f"Chat ID: {message.chat.id}, "
+            f"Message: {message.text}, "
+            f"Channel ID: {channel.id}, "
+            f"Channel Title: {channel.title}"
+        )
         return await message.answer(i18n.get("channel_not_found"))
 
     await state.update_data(channel_id=channel.id)
@@ -145,8 +245,27 @@ async def enter_update_time(
         return
     if not message.text.isdigit():
         return
+    data = await state.get_data()
+
+    logger.info(
+        "Admin update channel time , "
+        f"User ID: {message.from_user.id}, "  # type: ignore
+        f"Username: {message.from_user.username}, "  # type: ignore
+        f"Chat ID: {message.chat.id}, "
+        f"Message: {message.text}, "
+        f"Channel ID: {data['channel_id']}"
+    )
 
     data = await state.get_data()
     await channel_repository.update(int(data["channel_id"]), post_interval=message.text)
     await message.answer(i18n.get("channel_post_interval_updated"))
     await state.clear()
+
+    logger.info(
+        "Admin successfully updated channel time , "
+        f"User ID: {message.from_user.id}, "  # type: ignore
+        f"Username: {message.from_user.username}, "  # type: ignore
+        f"Chat ID: {message.chat.id}, "
+        f"Message: {message.text}, "
+        f"Channel ID: {data['channel_id']}"
+    )
--- src/bot/routers/admin/menu.py
+++ src/bot/routers/admin/menu.py
@@ -1,10 +1,8 @@
-from aiogram import F, Router
+from aiogram import Router
 from aiogram.filters import Command
-from aiogram.types import CallbackQuery, Message
+from aiogram.types import Message
 from aiogram_i18n import I18nContext
-from bot.keyboards.admin import inline_keyboards
-from bot.keyboards.admin.callback_types import AdminMenu
-from bot.keyboards.admin.callback_values import AdminMenuActions
+from bot.dependencies import logger
 
 
 router = Router(name="menu")
@@ -12,20 +10,11 @@ router = Router(name="menu")
 
 @router.message(Command("admin"))
 async def admin_menu(message: Message, i18n: I18nContext):
-    await message.answer(
-        text=i18n.get("admin_menu"),
-        reply_markup=inline_keyboards.back_to_admin_menu(),
+    logger.info(
+        "Admin opened admin menu, "
+        f"User ID: {message.from_user.id}, "  # type: ignore
+        f"Username: {message.from_user.username}, "  # type: ignore
+        f"Chat ID: {message.chat.id},  "
+        f"Message: {message.text}"
     )
-
-
-@router.callback_query(
-    AdminMenu.filter(F.action == AdminMenuActions.BACK_TO_ADMIN_MENU)
-)
-async def back_to_admin_menu(call: CallbackQuery, i18n: I18nContext):
-    if call.message is None:
-        return
-
-    await call.message.answer(
-        text=i18n.get("admin_menu"),
-        reply_markup=inline_keyboards.back_to_admin_menu(),
-    )
+    await message.answer(text=i18n.get("admin_menu"))
