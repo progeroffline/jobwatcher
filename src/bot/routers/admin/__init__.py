a87e3bfa8a4f47726f0bad0475adb4ab0b0abde8
progeroffline <progeroffline@gmail.com>
2024-11-12 15:40:26 +0200

    Include notify users rotuer

--- src/bot/routers/admin/__init__.py
+++ src/bot/routers/admin/__init__.py
@@ -2,10 +2,12 @@ from aiogram import Router
 from bot.filters.admin import IsAdminFilter
 from .menu import router as menu_router
 from .channels import router as channels_router
+from .notify_users import router as notify_users_router
 
 router = Router(name="admin")
 router.include_router(menu_router)
 router.include_router(channels_router)
+router.include_router(notify_users_router)
 
 router.message.filter(IsAdminFilter())
 router.callback_query.filter(IsAdminFilter())
