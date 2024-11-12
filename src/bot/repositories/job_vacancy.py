dd29375341d61b09276549829ceabc44ce2a7e8c
progeroffline <progeroffline@gmail.com>
2024-11-12 03:05:30 +0200

    Remove print and make get method

--- src/bot/repositories/job_vacancy.py
+++ src/bot/repositories/job_vacancy.py
@@ -1,11 +1,20 @@
+from sqlalchemy import select
 from bot.database.models import JobVacancy
 from bot.repositories.abstracts import BaseRepository
 
 
 class JobVacancyRepository(BaseRepository):
     async def create(self, **kwargs) -> JobVacancy:
-        print("Create by kwars", kwargs)
+        job_vacancy = await self.get(id=kwargs["id"])
+        if job_vacancy is not None:
+            return job_vacancy  # Return value if it exists in db
+
         job_vacancy = JobVacancy(**kwargs)
         self._session.add(job_vacancy)
         await self._session.commit()
         return job_vacancy
+
+    async def get(self, id: str) -> JobVacancy | None:
+        stmt = select(JobVacancy).where(JobVacancy.id == id)
+        result = await self._session.execute(stmt)
+        return result.scalar_one_or_none()
