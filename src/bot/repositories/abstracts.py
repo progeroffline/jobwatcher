0a214a0a3fbbcc346ffe65f2ab730158e3c68f2c
progeroffline <progeroffline@gmail.com>
2024-11-12 15:40:04 +0200

    Use session with context manager

--- src/bot/repositories/abstracts.py
+++ src/bot/repositories/abstracts.py
@@ -1,4 +1,3 @@
-import asyncio
 from abc import ABC
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -6,10 +5,3 @@ from sqlalchemy.ext.asyncio import AsyncSession
 class BaseRepository(ABC):
     def __init__(self, session: AsyncSession):
         self._session = session
-
-    def __del__(self):
-        try:
-            loop = asyncio.get_running_loop()
-            loop.create_task(self._session.close())
-        except RuntimeError:
-            asyncio.run(self._session.close())
--- src/bot/utils/new_jobs_notifications/__init__.py
+++ src/bot/utils/new_jobs_notifications/__init__.py
@@ -1,6 +1,7 @@
 import asyncio
 from datetime import datetime
 from aiogram import Bot
+from aiogram.exceptions import TelegramForbiddenError
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 from sqlalchemy.ext.asyncio.session import async_sessionmaker
@@ -8,6 +9,7 @@ from sqlalchemy.ext.asyncio.session import async_sessionmaker
 from bot.repositories.channel import ChannelRepository
 from bot.repositories.job_vacancy import JobVacancyRepository
 from bot.repositories.user import UserRepository
+from bot.dependencies import settings
 from . import artstation, belmeta, jobsua, olx, rabotaua, workaua
 from .notifications_queue import notifications_queue
 
@@ -16,9 +18,6 @@ class NewJobsNotifications:
     def __init__(self, sessionmaker: async_sessionmaker):
         self.sessionmaker = sessionmaker
         self.scheduler = AsyncIOScheduler()
-        self.job_vacancy_repository = JobVacancyRepository(sessionmaker())
-        self.user_repository = UserRepository(sessionmaker())
-        self.channel_repository = ChannelRepository(sessionmaker())
         self.first_launch = True
 
     async def send_notification(
@@ -27,31 +26,42 @@ class NewJobsNotifications:
         user_id: int,
         vacancy: dict[str, str | int],
     ) -> None:
-        await bot.send_message(chat_id=user_id, text=str(vacancy["title"]))
+        if settings.noitify_users_about_new_vacancies:
+            await bot.send_message(chat_id=user_id, text=str(vacancy["title"]))
         await asyncio.sleep(0.1)
 
     async def send_notifications_for_job(self, bot: Bot) -> None:
-        while True:
-            vacancy = await notifications_queue.get()
+        async with self.sessionmaker() as session:
+            user_repository = UserRepository(session)
+            while True:
+                vacancy = await notifications_queue.get()
+                notifications_queue.task_done()
 
-            if not self.first_launch:
-                async for user_id in self.user_repository.get_all():
-                    await self.send_notification(bot, user_id, vacancy)
+                if self.first_launch:
+                    continue
 
-            notifications_queue.task_done()
+                async for user_id in user_repository.get_all():
+                    try:
+                        await self.send_notification(bot, user_id, vacancy)
+                    except TelegramForbiddenError:
+                        pass
+                    except Exception as e:
+                        print(e)
 
     async def make_post_to_channel(self, bot: Bot) -> None:
-        channels = await self.channel_repository.get_all()
-        vacancies = await self.job_vacancy_repository.get_not_sent_to_channel()
-        now = datetime.now()
+        async with self.sessionmaker() as session:
+            job_vacancy_repository = JobVacancyRepository(session)
+            channel_repository = ChannelRepository(session)
+            channels = await channel_repository.get_all()
+            vacancies = await job_vacancy_repository.get_not_sent_to_channel()
+            now = datetime.now()
 
-        for channel in channels:
-            if now.minute % channel.post_interval == 0:
+            for channel in channels:
+                if now.minute % channel.post_interval != 0:
+                    continue
                 for vacancy in vacancies:
                     await self.send_notification(bot, channel.id, vacancy.to_dict())
-                    await self.job_vacancy_repository.mark_as_sent_to_channel(
-                        vacancy.id
-                    )
+                    await job_vacancy_repository.mark_as_sent_to_channel(vacancy.id)
                     await asyncio.sleep(3)
 
     async def scrap_data(self) -> None:
