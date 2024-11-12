b23f9c7effa8d2a5f6e98124ecf3b0f777fdc791
progeroffline <progeroffline@gmail.com>
2024-11-12 16:42:15 +0200

    Create locations and connect it to job vacancies by id

--- src/bot/database/models.py
+++ src/bot/database/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy import Boolean, BigInteger, Integer, String, Text, inspect
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Boolean, BigInteger, ForeignKey, Integer, String, Text, inspect
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from bot.database.abstracts import ModelPrettyPrint
 
 
@@ -34,8 +34,37 @@ class JobVacancy(ModelPrettyPrint):
     salary_currency: Mapped[str] = mapped_column(String, default="")
     salary_period: Mapped[str] = mapped_column(String, default="")
     sent_to_channel: Mapped[bool] = mapped_column(Boolean, default=False)
-
     url: Mapped[str] = mapped_column(String, default="")
+
+    location_id: Mapped[int] = mapped_column(ForeignKey("job_vacancies_locations.id"))
+    location: Mapped["JobVacancyLocation"] = relationship(
+        "JobVacancyLocation", back_populates="vacancies"
+    )
+
+    def to_dict(self):
+        data = {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
+        if self.location:
+            data["location"] = self.location.to_dict()
+        return data
+
+
+class JobVacancyLocation(ModelPrettyPrint):
+    __tablename__ = "job_vacancies_locations"
+
+    id: Mapped[int] = mapped_column(
+        Integer,
+        primary_key=True,
+        unique=True,
+        autoincrement=True,
+    )
+    continent: Mapped[str] = mapped_column(String, default="")
+    country: Mapped[str] = mapped_column(String, default="")
+    city: Mapped[str] = mapped_column(String, default="")
+
+    vacancies: Mapped[list[JobVacancy]] = relationship(
+        "JobVacancy",
+        back_populates="location",
+    )
 
     def to_dict(self):
         return {c.key: getattr(self, c.key) for c in inspect(self).mapper.column_attrs}
