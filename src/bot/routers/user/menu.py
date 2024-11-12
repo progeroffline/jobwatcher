40c23b96f2e0966211b08910486895fce3aa47c3
progeroffline <progeroffline@gmail.com>
2024-11-12 15:53:25 +0200

    Add logs ot toher files

--- src/bot/routers/user/menu.py
+++ src/bot/routers/user/menu.py
@@ -1,13 +1,18 @@
 from aiogram import Router
-from aiogram.types import CallbackQuery, InaccessibleMessage, Message
+from aiogram.types import Message
 from aiogram_i18n import I18nContext
+from bot.dependencies import logger
 
 
 router = Router(name="menu")
 
 
-async def user_welcome_message(
-    message: Message | InaccessibleMessage | CallbackQuery,
-    i18n: I18nContext,
-):
+async def user_welcome_message(message: Message, i18n: I18nContext):
+    logger.info(
+        "User opened menu, "
+        f"User ID: {message.from_user.id}, "  # type: ignore
+        f"Username: {message.from_user.username}, "  # type: ignore
+        f"Chat ID: {message.chat.id},  "
+        f"Message: {message.text}"
+    )
     await message.answer(i18n.get("welcome_message"))
--- src/bot/utils/new_jobs_notifications/__init__.py
+++ src/bot/utils/new_jobs_notifications/__init__.py
@@ -9,13 +9,14 @@ from sqlalchemy.ext.asyncio.session import async_sessionmaker
 from bot.repositories.channel import ChannelRepository
 from bot.repositories.job_vacancy import JobVacancyRepository
 from bot.repositories.user import UserRepository
-from bot.dependencies import settings
+from bot.dependencies import settings, logger
 from . import artstation, belmeta, jobsua, olx, rabotaua, workaua
 from .notifications_queue import notifications_queue
 
 
 class NewJobsNotifications:
     def __init__(self, sessionmaker: async_sessionmaker):
+        logger.debug("Init NewJobsNotifications service")
         self.sessionmaker = sessionmaker
         self.scheduler = AsyncIOScheduler()
         self.first_launch = True
@@ -31,24 +32,35 @@ class NewJobsNotifications:
         await asyncio.sleep(0.1)
 
     async def send_notifications_for_job(self, bot: Bot) -> None:
+        logger.debug("Send notifications about new job to users")
         async with self.sessionmaker() as session:
             user_repository = UserRepository(session)
             while True:
                 vacancy = await notifications_queue.get()
+                logger.debug(f"Got new vacancy for queue {vacancy}")
+                logger.debug(f"Remove vacancy from queue {vacancy}")
                 notifications_queue.task_done()
 
                 if self.first_launch:
+                    logger.debug("Skip sending notification cause it is a first launch")
                     continue
 
                 async for user_id in user_repository.get_all():
                     try:
                         await self.send_notification(bot, user_id, vacancy)
                     except TelegramForbiddenError:
-                        pass
-                    except Exception as e:
-                        print(e)
+                        logger.debug(
+                            f"Skip sending notification cause bot blocked by user ID: {user_id}"
+                        )
+                    except Exception as error:
+                        logger.error(
+                            f"Got error when trying sent notification to user ser ID: {user_id},"
+                            f"Error: {error}"
+                        )
+                        print(error)
 
     async def make_post_to_channel(self, bot: Bot) -> None:
+        logger.debug("Send notifications about new job to channels")
         async with self.sessionmaker() as session:
             job_vacancy_repository = JobVacancyRepository(session)
             channel_repository = ChannelRepository(session)
@@ -57,24 +69,53 @@ class NewJobsNotifications:
             now = datetime.now()
 
             for channel in channels:
+                logger.debug(
+                    f"Check channel ID: {channel.id}, "
+                    f"Title: {channel.title}, "
+                    f"Post interval: {channel.post_interval}"
+                )
                 if now.minute % channel.post_interval != 0:
+                    logger.debug(
+                        f"Skip channel ID: {channel.id}, "
+                        f"Title: {channel.title}, "
+                        f"Post interval: {channel.post_interval}"
+                    )
+
                     continue
                 for vacancy in vacancies:
+                    logger.debug(
+                        f"Send vacancies to channel ID: {channel.id}, "
+                        f"Title: {channel.title}, "
+                        f"Post interval: {channel.post_interval}"
+                    )
                     await self.send_notification(bot, channel.id, vacancy.to_dict())
                     await job_vacancy_repository.mark_as_sent_to_channel(vacancy.id)
                     await asyncio.sleep(3)
 
     async def scrap_data(self) -> None:
+        logger.debug("Scrap vacancies from sites")
         async with self.sessionmaker() as session:
+            logger.debug("Scrap vacancies from artstation site")
             await artstation.scrap_data(JobVacancyRepository(session))
+
+            logger.debug("Scrap vacancies from belmeta site")
             await belmeta.scrap_data(JobVacancyRepository(session))
+
+            logger.debug("Scrap vacancies from jobsua site")
             await jobsua.scrap_data(JobVacancyRepository(session))
+
+            logger.debug("Scrap vacancies from olx site")
             await olx.scrap_data(JobVacancyRepository(session))
+
+            logger.debug("Scrap vacancies from rabotaua site")
             await rabotaua.scrap_data(JobVacancyRepository(session))
+
+            logger.debug("Scrap vacancies from workaua site")
             await workaua.scrap_data(JobVacancyRepository(session))
             self.first_launch = False
 
     async def start(self, bot: Bot) -> None:
+        logger.debug("Create tasks for NewJobsNotifications service")
         self.scheduler.add_job(
             self.scrap_data,
             trigger=IntervalTrigger(minutes=1),
@@ -86,5 +127,8 @@ class NewJobsNotifications:
             minutes=2,
             args=(bot,),
         )
+        logger.debug("Start scheduler for NewJobsNotifications service")
         self.scheduler.start()
+
+        logger.debug("Bind send_notifications_for_job for NewJobsNotifications service")
         asyncio.create_task(self.send_notifications_for_job(bot))
