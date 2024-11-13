d8f617934fa4076a8e71f3d1cc4a1a39cb2b27a6
progeroffline <progeroffline@gmail.com>
2024-11-13 04:14:05 +0200

    Add test to ignore file

--- src/bot/tests/services_rabotaua.py
+++
@@ -1,9 +0,0 @@
-from rich import print
-from bot.services.rabotaua import RabotaUAParser
-
-
-async def test():
-    service = RabotaUAParser()
-
-    response = await service.search()
-    print(response)
