7969ef3df65532d4e1932697853447b608121234
progeroffline <progeroffline@gmail.com>
2024-11-15 02:24:55 +0200

    Fix wrong variable name

--- src/bot/utils/new_jobs_notifications/artstation.py
+++ src/bot/utils/new_jobs_notifications/artstation.py
@@ -3,18 +3,18 @@ from bot.services.artstation import ArtStationParser
 from .notifications_queue import notifications_queue
 
 
-async def scrap_data(job_vacancy_respository: JobVacancyRepository):
+async def scrap_data(job_vacancy_repository: JobVacancyRepository):
     service = ArtStationParser()
     response = await service.search()
 
     for job_vacancy in response:
-        service_id = await job_vacancy_respository.get_service_id(
+        service_id = await job_vacancy_repository.get_service_id(
             service_name="artstation",
             service_id=job_vacancy["category"]["service_id"],  # type: ignore
         )
-        category = await job_vacancy_respository.get_category(id=service_id.category_id)
+        category = await job_vacancy_repository.get_category(id=service_id.category_id)
         job_vacancy["category"]["name"] = category.name  # type: ignore
 
-        if not await job_vacancy_respository.exists(str(job_vacancy["id"])):
-            await job_vacancy_respository.create(**job_vacancy)  # type: ignore
+        if not await job_vacancy_repository.exists(str(job_vacancy["id"])):
+            await job_vacancy_repository.create(**job_vacancy)  # type: ignore
             await notifications_queue.put(job_vacancy)
